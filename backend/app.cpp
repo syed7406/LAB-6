08672e24c2a0286f09713d086dbc531db8fdb226
Syed abdul hameed <hameedsyed790@gmail.com>
2026-02-20 11:15:12 +0530

    Updated app to run continuously

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+#include <unistd.h>
+
+int main() {
+    std::cout << "Hello from Jenkins CI/CD Lab 6!" << std::endl;
+
+    while(true) {
+        sleep(5);
+        std::cout << "Application Running..." << std::endl;
+    }
+
+    return 0;
+}#include <iostream>
 
 int main() {
     std::cout << "Hello from Jenkins CI/CD Lab 6!" << std::endl;
